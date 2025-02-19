1df6154feebc8fb6ed6e8488134e35b937c396a7
ViniciusAugustoo <vinicius.x.pirata@gmail.com>
2025-02-19 16:06:27 -0300

    apenas mais um commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
- const colors = ['Red','Blue', 'Yellow', 'Purple', 'Green', 'black', 'white']
- const btn = document.getElementById('btn')
- const color = document.querySelector ('.color')
+ const colors = ['Red','Blue', 'Yellow', 'Purple', 'Green', 'black', 'white'] // cosntante criando as cores
+ const btn = document.getElementById('btn') //fazendo com que o change color se torne um botao funcional 
+ const color = document.querySelector ('.color') // fazendo com que o nome das cores aparecça em your color 
 
- btn.addEventListener('click', function() {
-     const RandomColor = getRandomColor()
-     document.body.style.background = colors [RandomColor]
-    console.log(RandomColor)
-    color.textContent = colors[RandomColor]
+ btn.addEventListener('click', function() { // função para o botao funcionar da forma correta
+     const RandomColor = getRandomColor() // random feita para as cores da minha array ser selecionada
+     document.body.style.background = colors [RandomColor] 
+    console.log(RandomColor) // apenas um print das posiçoes da minha array
+    color.textContent = colors[RandomColor] //aparecendo o nome do texto das colors
  })
 
- function getRandomColor () {
+ function getRandomColor () { // funçã para apenas escolher os numeros que estiverem dentro da minha array 
     return Math.floor (Math.random() * colors.length)
  }
 
